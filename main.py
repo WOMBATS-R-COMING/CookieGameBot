1e385ce36de1c2a38930b549e65cd0757bfc89b6
Maciek <zurowski.maciek@gmail.com>
2022-08-31 12:59:32 +0200

    baby step

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+print("hi")
